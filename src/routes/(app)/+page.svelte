b9d5ebb4c710ab90dc447b9a1bf887c2876e4ecf
Nelit <nelit2714@gmail.com>
2025-01-17 15:22:02 +0100

    chore(seo): add title and description meta tag for home page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,5 +2,10 @@
 	import NetworkManagementForm from "$lib/components/+networkManagementForm.svelte";
 </script>
 
+<svelte:head>
+	<title>Netify — Subnetting Tool | Calculate Subnet Masks, CIDR, Hosts, and Network Address</title>
+	<meta name="description" content="Use our subnetting tool to easily calculate subnet masks, CIDR prefixes, host ranges, and network addresses. Split your network into subnets with custom host requirements and optimize your IP address management.">
+</svelte:head>
+
 <NetworkManagementForm/>
 
