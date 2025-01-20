833b1915a0de88827b813bb223a9e9c2c3eeb914
Nelit <nelit2714@gmail.com>
2025-01-20 13:35:13 +0100

    chore: add props sectionID for section component

--- src/lib/components/ui/+section.svelte
+++ src/lib/components/ui/+section.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let title;
+	export let sectionID;
 </script>
 
-<section class="container mx-auto max-w-[1240px]">
-	<div class="text-center">
-		<h1 class="text-white font-bold text-4xl">{title}</h1>
-	</div>
+<section class="container mx-auto max-w-[1240px]" id={sectionID}>
+	{#if title}
+		<div class="text-center">
+			<h1 class="text-white font-bold text-4xl">{title}</h1>
+		</div>
+	{/if}
+
 	<slot />
 </section>
