208277c6cddc55c9d5baf4766cfbd7dcebadec5e
Nelit <nelit2714@gmail.com>
2025-01-20 12:26:23 +0100

    style(feature): change styles in feature component

--- src/lib/components/ui/+feature.svelte
+++ src/lib/components/ui/+feature.svelte
@@ -3,18 +3,20 @@
 </script>
 
 <div
-	class="bg-[#161616] p-5 rounded-[30px] flex flex-col items-center w-[300px] gap-4 border-2 border-[#39965F] cursor-pointer active:-translate-y-[-10px] duration-[0.4s] lg:hover:translate-y-[-10px]">
-	<div>
-		<img src={data.icon} class="w-[60px]" alt="" />
-	</div>
-	<div class="flex flex-col justify-between gap-1">
-		<div class="h-[50px] flex flex-col justify-center">
-			<h2 class="text-white text-center">{data.title}</h2>
-		</div>
+	class="bg-[#171717] p-5 rounded-[15px] flex flex-col gap-3 border-2 border-[#1B1B1B] cursor-pointer duration-[0.4s]
+			active:translate-y-[-10px] active:border-[#39965F]
+			lg:hover:translate-y-[-10px] lg:hover:border-[#39965F]">
+	<div class="flex gap-3">
 		<div>
-			<p class="text-white text-center font-light text-[rgba(255,255,255,0.7)]">
-				{data.description}
-			</p>
+			<img src={data.icon} class="w-[60px]" alt="" />
 		</div>
+		<div class="flex flex-col">
+			<h2 class="text-white">{data.title}</h2>
+		</div>
+	</div>
+	<div>
+		<p class="text-white font-light text-[rgba(255,255,255,0.7)]">
+			{data.description}
+		</p>
 	</div>
 </div>
